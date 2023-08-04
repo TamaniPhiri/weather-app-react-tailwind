2597d52775d4e8277be29522b9a0d166aa35bb97
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-04 11:32:19 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-const APIkey="db5595bf66ed081a4a8bc0aff8227211"
+const APIkey = "db5595bf66ed081a4a8bc0aff8227211";
 
 function App() {
- const[data,setData]=useState(null)
- const[location,setLocation]=useState('Zambia');
- useEffect(()=>{
-  const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`
-  axios.get(url).then(res=>{
-    setData(res.data)
-  })
- },[location])
- console.log(data);
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState("Zambia");
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`;
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  }, [location]);
+  console.log(data);
+
+  if (!data) {
+    return (
+      <div className="flex min-h-screen w-full items-center justify-center">
+        <div className="w-32 h-32 rounded-full border-8 border-t-teal-600 animate-spin"></div>
+      </div>
+    );
+  }
+
   return (
     <>
       <p>hello</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/icons/Haze.jsx
@@ -0,0 +1,15 @@
+function IconCloudHaze2(props) {
+  return (
+    <svg
+      fill="currentColor"
+      viewBox="0 0 16 16"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M8.5 3a4.002 4.002 0 00-3.8 2.745.5.5 0 11-.949-.313 5.002 5.002 0 019.654.595A3 3 0 0113 12H4.5a.5.5 0 010-1H13a2 2 0 00.001-4h-.026a.5.5 0 01-.5-.445A4 4 0 008.5 3zM0 7.5A.5.5 0 01.5 7h5a.5.5 0 010 1h-5a.5.5 0 01-.5-.5zm2 2a.5.5 0 01.5-.5h9a.5.5 0 010 1h-9a.5.5 0 01-.5-.5zm-2 4a.5.5 0 01.5-.5h9a.5.5 0 010 1h-9a.5.5 0 01-.5-.5z" />
+    </svg>
+  );
+}
+
+export default IconCloudHaze2;
